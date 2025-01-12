39de8415fd9cec3c53051b73285e7f2c6b1d8a9d
Santosh <pantsantosh23@gmail.com>
2025-01-11 21:28:58 -0600

    Backend Init COde

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3002;
+const zod = require('zod');
 
 const mongoose  = require('mongoose');
 const {User, Todo} = require('./schema');
@@ -13,34 +14,56 @@ app.get("/", (req, res)=>{
     res.send("Welcome to Backend World");
 })
 
-app.post("/login", async (req, res)=>{
-   const email = req.body.email;
+
+function checkAuthDetails(req, res, next){
+   try{
+    const loginData = zod.object({
+        email: zod.string().email("Please give valid email address"),
+        password: zod.string().min(6, "password should be at least 6 characters")
+    })
+
+    const userGaveMeThisData = req.body;
+
+    loginData.parse(userGaveMeThisData);
+    next();
+   } catch(error){
+    return res.send(error.errors.map((e) => e.message).join(", "));
+   }
+}
+
+//try catch
+
+app.post("/login", checkAuthDetails,  async (req, res)=>{
+   try{
+    const email = req.body.email;
    const password = req.body.password;
 
-   if(!email){
-    return res.send("Email Cannot be empty");
-   }
-
-   if(!password){
-   return  res.send("Password is required");
-   }
-
    const findUser = await User.findOne({email: email, password: password});
+
    if(!findUser){
     return res.send("No User found");
    }
 
    return res.send("Success");
+   } catch(error){
+    return res.send("Sorry, something went wrong, try again");
+   }
 
 })
 
-app.post("/signup", async (req, res)=>{
+app.post("/signup", checkAuthDetails, async (req, res)=>{
+   try{
     const email = req.body.email;
     const password = req.body.password;
 
     const newPerson = new User({email: email, password: password});
+
     await newPerson.save();
+
     res.send("Successfully registered");
+   } catch(error){
+    res.send("Sorry, something went wrong, try again");
+   }
 })
 
 app.post("/maketodo", async (req, res)=>{
@@ -54,17 +77,45 @@ app.post("/maketodo", async (req, res)=>{
     return res.send("Successfully added new todo");
 })
 
-app.post("/deletetodo", (req, res)=>{
-    res.send("I am deletetodo endpoint");
+app.post("/deletetodo", async (req, res)=>{
+    const id = req.body.id;
+
+    await Todo.deleteOne({_id: id});
+    return res.send("Deleted Successfully");
 })
 
-app.post("/updatetodo", (req, res)=>{
-    res.send("I am updatetodo endpoint");
+app.post("/updatetodo", async (req, res)=>{
+    const {title, description, id} = req.body;
+
+    await Todo.updateOne({_id: id}, {title: title, description: description});
+    return res.send("Updated :))");
 })
 
 
+app.post("/markdone", async (req, res)=>{
+const id = req.body.id;
+
+await Todo.updateOne({_id: id}, {isDone: true});
+return res.send("Marked as Done :))")
+
+})
+
+
+app.get("/gettodos", async (req, res)=>{
+
+    const allTodos = await Todo.find({});
+    return res.json({
+        "todos": allTodos,
+        "message": "All Todos are returned to user"
+    })
+})
 
 app.listen(port, ()=>{
     console.log("Healthy Server");
     
 })
+
+
+
+//CRUD - Creating, Reading, Updating, and Deleting
+//Middlewares
